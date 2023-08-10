bd20d6b1e24c3d2c64bdea5b59cb8426f9eeb5d7
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-10 10:58:34 -0700

    First c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
-* main
+* main - Where we start
+* Short Description - It is just a printing function.
 * Description: Prints "Programming is like building a multilingual puzzle
-* return-> 0
+* to the std output
+* Return: 0
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
