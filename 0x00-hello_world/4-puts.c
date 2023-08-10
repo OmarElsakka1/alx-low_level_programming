2c8da6e901a7de6ed6b1e26d2f2235f5c01c6cef
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-10 10:44:15 -0700

    Committing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+/**
+This function is a function that prints a sentence Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
