db9071bfb49f271144354c681bfde40d52bd6790
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-10 10:50:26 -0700

    First c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
- * This function is a function that prints a sentence Programming is like building a multilingual puzzle
- */
+* Description: Prints "Programming is like building a multilingual puzzle
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
