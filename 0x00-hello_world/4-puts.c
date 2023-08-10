52de6fdfa5a2ecb013b9afaca1a68e38b28aebfc
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-10 10:48:34 -0700

    First c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
-This function is a function that prints a sentence Programming is like building a multilingual puzzle
+ * This function is a function that prints a sentence Programming is like building a multilingual puzzle
  */
 int main(void)
 {
