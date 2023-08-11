a4a95b269b74aca9fb16060fbed1cbb060683065
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-11 04:15:05 -0700

    Printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - Where we start
+* Short Description - It is just a printing function.
+* Description: Prints with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
