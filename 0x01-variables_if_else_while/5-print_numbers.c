1b3947f4703483383fdc6702852c6b79840223bc
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-11 17:55:29 -0700

    Printing numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - We are starting right here
+* Description: Prints numbers from 0 to 10.
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
