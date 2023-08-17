52bdfa8294ad39500778f80adda206a41adc5ed1
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-17 16:23:32 -0700

    Printing square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - prints
+ * @size: square length
+ * Return: 0
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			if (i != size - 1)
+				_putchar('\n');
+		}
+	_putchar('\n');
+}
