a60a6959aa4281275e1e5d1f504f369a89fd6b56
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-17 16:25:16 -0700

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: fizz-bizz
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
