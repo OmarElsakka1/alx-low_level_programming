c2faeb665f8a67c64d201d3a65c99d51bf7a381f
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-17 16:14:02 -0700

    Printing most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints from 0 -> 9
+ * Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
