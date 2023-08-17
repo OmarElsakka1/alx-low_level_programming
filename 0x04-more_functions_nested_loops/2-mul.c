97ba6283f7f45caa0e051cc323b1605215093a15
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-17 16:05:50 -0700

    multipy 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiply two numbers
+ * @a: First number
+ * @b: Second number
+ * Description: multiply two numbers
+ * Return: multiplication of a and b
+*/
+
+int mul(int a, int b)
+{
+	return a*b;
+}
