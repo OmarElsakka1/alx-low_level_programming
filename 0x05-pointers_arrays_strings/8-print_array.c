587ae36f9018ae572f496af1b3d947e1d8f2d31b
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-22 16:43:55 -0700

    Printing array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_array - prints 1D int array
+* @a: array
+* @n: length of array
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
