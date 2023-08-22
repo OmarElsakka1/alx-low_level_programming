7faad555a422522cf61f469ef87b264d4a8ff9bb
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-22 16:39:18 -0700

    Printing even indices

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* puts2 - PUTS EVEN INNDECIES ONLY
+* @str: string to be printed
+* Return: void
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
