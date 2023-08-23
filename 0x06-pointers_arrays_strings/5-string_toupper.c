060a8030877b405f05f11e140d90f2e315b4724a
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-23 02:17:35 -0700

    Convert string to make it uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+* *string_toupper - convert to upper
+* @s: string
+* Return: new string
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+
+	return (s);
+}
