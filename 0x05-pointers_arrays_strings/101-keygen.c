dace6931896156eadc6ee188f0893e3d70693fa3
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-08-22 17:05:13 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: 0
+*/
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
