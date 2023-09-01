3a1076bf032012efc3a111a547f243d207ee65d0
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-09-01 15:46:01 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: params
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+	(void) argv;
+}
+ 
