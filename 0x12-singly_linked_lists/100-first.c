35d50fdff88fec683d16b417710e83a3f9afe5a4
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-09-21 18:56:54 -0700

    printing

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * __constructor - runs before main
+ * Return: void
+*/
+
+void __constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
