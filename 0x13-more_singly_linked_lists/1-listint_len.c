fa83105d3f0a193349f221325059a2faf4a14c50
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-09-25 10:59:59 -0700

    Return the length of linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - Returns number of elements in a type listint_t linked list
+ * @h: Pointer to the linked list head
+ * Return: Number of elements in the linked list
+ *
+ * Code by Omar El-Sakka
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number_of_nodes;
+
+	number_of_nodes = 0;
+
+	while (h)
+	{
+		number_of_nodes++;
+
+		h = h->next;
+	}
+	return (number_of_nodes);
+}
