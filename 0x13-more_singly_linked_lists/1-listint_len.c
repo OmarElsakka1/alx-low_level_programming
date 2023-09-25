4a751f497af0674752dfb63eba7e8215daa65f8b
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-09-25 11:06:06 -0700

    Return the length of the linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,11 +12,13 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t number_of_nodes;
+
 	number_of_nodes = 0;
 
 	while (h)
 	{
 		number_of_nodes++;
+
 		h = h->next;
 	}
 	return (number_of_nodes);
