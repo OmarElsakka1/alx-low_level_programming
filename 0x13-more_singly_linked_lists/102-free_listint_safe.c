966e60f5cda7b3f7fd59e3c6638d04c9e8f87461
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-09-26 07:43:34 -0700

    Freeing a linked list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,72 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list safely
- * @h: pointer to the head of  the linked list
+ * free_listp2 - freeing a linked list
+ * @head: head of the linked list.
  *
- * Return: number of elements in the freed list
- * Made by: Omar El-Sakka
+ * Return: Come on it is void function
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *ptr_temp;
+	listp_t *ptr_curr;
+
+	if (head != NULL)
+	{
+		ptr_curr = *head;
+		while ((ptr_temp = ptr_curr) != NULL)
+		{
+			ptr_curr = ptr_curr->next;
+			free(ptr_temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - freeing  The linked list safely.
+ * @h: head of the linked list.
+ *
+ * Return: size of the linked list.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t length = 0;
-	int difference;
-	listint_t *ptr_temp;
+	size_t num_nodes = 0;
+	listp_t *hptr, *ptr_new, *ptr_add;
+	listint_t *ptr_curr;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		difference = *h - (*h)->next;
-		if (difference > 0)
+		ptr_new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		ptr_new->p = (void *)*h;
+		ptr_new->next = hptr;
+		hptr = ptr_new;
+
+		ptr_add = hptr;
+
+		while (ptr_add->next != NULL)
 		{
-			ptr_temp = (*h)->next;
-			*h = ptr_temp;
-			length++;
+			ptr_add = ptr_add->next;
+			if (*h == ptr_add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (num_nodes);
+			}
 		}
-		else
-		{
-			*h = NULL;
-			length++;
-			break;
-		}
+
+		ptr_curr = *h;
+		*h = (*h)->next;
+		free(ptr_curr);
+		num_nodes++;
 	}
 
 	*h = NULL;
-
-	return (length);
+	free_listp2(&hptr);
+	return (num_nodes);
 }
