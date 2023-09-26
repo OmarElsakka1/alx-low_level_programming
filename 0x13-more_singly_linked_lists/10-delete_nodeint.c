c2a23983d6c9bc96c41a2b3a275d73f6392e5c0c
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-09-26 12:47:22 -0700

    Deleting a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,44 +1,43 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deleteing a certain node in the given linked list
- * @head: pointer to the head of the linked list
- * @index: index of the nod in the linked list to be  deleted
- * Return: 1 -> sucess, -1 -> Fail
- * Made by: Omar El-Sakka
+ * delete_nodeint_at_index - deleting a certain node in the linked list
+ * @head: Address the head of a list
+ * @index: index of the node in the linked list
+ * Return: 1 -> success, -1 -> failed
  */
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *previous;
+	listint_t *previous_ptr;
 	listint_t *next;
 
-	previous = *head;
+	previous_ptr = *head;
 
 	if (index != 0)
 	{
 		for (i = 0; i < index - 1 && prev != NULL; i++)
 		{
-			previous = previous->next;
+			previous_ptr = previous_ptr->next;
 		}
 	}
 
-	if (previous == NULL || (previous->next == NULL && index != 0))
+	if (previous_ptr == NULL || (previous_ptr->next == NULL && index != 0))
 	{
 		return (-1);
 	}
 
-	next = previous->next;
+	next = previous_ptr->next;
 
 	if (index != 0)
 	{
-		previous->next = next->next;
+		previous_ptr->next = next->next;
 		free(next);
 	}
 	else
 	{
-		free(previous);
+		free(previous_ptr);
 		*head = next;
 	}
 
