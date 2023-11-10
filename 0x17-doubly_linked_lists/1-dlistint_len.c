2bf1d22d1bb15af241c7bf668d0d16408a71ba31
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2023-11-10 01:04:59 -0800

    length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,12 +4,11 @@
  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
- * Return: The number of elements existing in the dlistint_t list.
- * Made by: Omar El-Sakka
+ * Return: The number of elements of the dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t num_nodes = 0;
+	size_t new_nodes = 0;
 
 	while (h)
 	{
