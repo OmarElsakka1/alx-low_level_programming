df67c327c7ade1affcafc7a8b85c51591b746127
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-03-14 02:48:06 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,17 +13,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-  size_t j;
+size_t j;
 
-  if (array == NULL)
-    return (-1);
+if (array == NULL)
+return (-1);
 
-  for (j = 0; j < size; j++)
-    {
-      printf("Value checked array[%ld] = [%d]\n", j, array[j]);
-      if (array[j] == value)
-	return (j);
-    }
+for (j = 0; j < size; j++)
+{
+printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+if (array[j] == value)
+return (j);
+}
 
-  return (-1);
+return (-1);
 }
